23b65a593d65165d9a0f300c4c5fbe3f2f7632d2
tinaLX <2168633941@qq.com>
2019-06-08 19:26:49 +0800

    git study.md

--- js/base.js
+++ js/base.js
@@ -41,15 +41,52 @@ function scroll(){
     }
 }
 
-function animate(obj,target){
+function animate(obj,json,fn){
     clearInterval(obj.timer);
-    var speed = target > obj.offsetLeft ? 5 : -5;
     obj.timer = setInterval(function(){
-        obj.style.left = obj.offsetLeft + speed + "px";
-        var result = target - obj.offsetLeft;
-        if(Math.abs(result) <= 5){
-            clearInterval(obj.timer);
-            obj.style.left = target + "px";
+        var flag = true;
+        for (var attr in json){
+            var current = 0;
+            if(attr == "opacity"){
+                current = Math.round(getStyle(obj,attr))*100 || 0;
+            }
+            else{
+                current = parseInt(getStyle(obj,attr));
+            }
+            var step = (json[attr] - current)/10;
+            step = step >0 ? Math.ceil(step) : Math.floor(step);
+            if(attr == "opacity"){
+                if("opacity" in obj.style){
+                    obj.style.opacity = (current + step)/100;
+                }
+                else{
+                    obj.style.filter = "alpha(opacity = "+(current+step)+")";
+                }
+            }
+            else if(attr == "zIndex"){
+                obj.style.zIndex = json[attr];
+            }
+            else{
+                obj.style[attr] = current + step + "px";
+            }
+            
+            if(current != json[attr]){
+                flag = false;
+            }
         }
-    },5);
+        if(flag){
+            clearInterval(obj.timer);
+            if(fn){
+                fn();
+            }
+        }
+    },30);
+}
+function getStyle(obj,attr){
+    if(obj.currentStyle){
+        return obj.currentStyle[attr];
+    }
+    else{
+        return window.getComputedStyle(obj,null)[attr];
+    }
 }
